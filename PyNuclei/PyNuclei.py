e8fae4b9159cf3d33561ce678f42d9c557ef407b
Noam Rathaus <noamr@ssd-disclosure.com>
2024-04-15 08:59:01 +0300

    Use improved version

--- PyNuclei/PyNuclei.py
+++ PyNuclei/PyNuclei.py
@@ -344,7 +344,7 @@ class Nuclei:
         except FileExistsError:
             pass
 
-    def return_templates_details_v2(self):
+    def return_templates_details(self):
         """
         Process the templates available and return them as a structure
         WARNING: This is a VERY time consuming function
@@ -410,56 +410,6 @@ class Nuclei:
                         del template_obj[key]
 
                 templates.append(template_obj)
-
-        return templates
-
-    def return_templates_details(self):
-        """
-        Process the templates available and return them as a structure
-        WARNING: This is a VERY time consuming function
-        """
-        nuclei_binary = "nuclei"
-        if self.nuclei_path:
-            nuclei_binary = f"{self.nuclei_path}/nuclei"
-        command = [nuclei_binary, "-no-color", "-template-display"]
-
-        process = subprocess.Popen(
-            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-
-        output, _ = process.communicate()
-        output = output.decode()
-
-        templates = []
-        # re is really slow to do matches - lets use simple 'find'
-        start_template = output.find("Template: ")
-        while start_template != -1:
-            # print(f"{len(templates)=:,}             \r", end="")
-            end_template = output.find("# digest: ", start_template)
-            if end_template == -1:
-                raise ValueError("Cannot find '# digest: ")
-
-            template = output[start_template:end_template]
-            if (
-                template.find("Template: ", len("Template: ")) != -1
-            ):  # len(...) = skip the first the name Template at the beginning
-                raise ValueError(
-                    "Template includes another Template inside it, is '# digest :' missing?"
-                )
-
-            template_obj = yaml.safe_load(template)
-
-            keys = list(template_obj.keys())
-            for key in keys:
-                # Keep only the info we want
-                if key not in ["Template", "id", "info"]:
-                    del template_obj[key]
-
-            templates.append(template_obj)
-
-            # Reducing the size of 'output' is very time consuming, we will avoid it
-            # output = output[end_template:]
-            start_template = output.find("Template: ", end_template)
 
         return templates
 
